e8d80b9df6323fffc34f7a5df3bc25d4d6107501
Abdul Ahad <ahadmughal812@gmail.com>
2024-09-30 14:13:31 +0500

    Applying changes

--- scrape.py
+++ scrape.py
@@ -1,9 +1,15 @@
 from selenium.webdriver import Remote, ChromeOptions
 from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
 from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
 
-AUTH = 'brd-customer-hl_35f00b16-zone-ai_scraper:b5zkxb1p947f'
-SBR_WEBDRIVER = f'https://{AUTH}@zproxy.lum-superproxy.io:9515'
+load_dotenv()
+
+# Access the AUTH token
+auth_token = os.getenv("AUTH")
+
+SBR_WEBDRIVER = f'https://{auth_token}@zproxy.lum-superproxy.io:9515'
 
 def scrape_website(url):
     print('Connecting to Scraping Browser...')
@@ -13,11 +19,11 @@ def scrape_website(url):
     with Remote(sbr_connection, options=ChromeOptions()) as driver:
         driver.get(url)
         print('waiting for captcha to solve')
-        # solve_res = driver.execute('executeCdpCommand', {
-        #     'cmd': 'Captcha.waitForSolve',
-        #     'params': {'detectTimeout': '10000'},
-        # })
-        # print('Captcha solved!', solve_res['value']['status'])
+        solve_res = driver.execute('executeCdpCommand', {
+            'cmd': 'Captcha.waitForSolve',
+            'params': {'detectTimeout': '10000'},
+        })
+        print('Captcha solved!', solve_res['value']['status'])
         print('Navigated! Scraping page content...')
         html = driver.page_source
         return html
